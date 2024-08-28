f199722c5177e8221f4bd4c5d38f22745648804b
gabrielmissio <gabriel.missio@compasso.com.br>
2024-08-28 11:13:00 -0300

    Optimize test setup by only initializing EVM node (Hardhat) for tests requiring RPC

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,4 @@ module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: ['js', 'mjs'],
   testMatch: ['**/tests/**/*.test.js', '**/tests/**/*.test.mjs'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 }
